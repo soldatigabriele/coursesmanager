cd1bb4274e975a49acceffda3ee6886341fe2631
Gabriele Soldati <soldati.gabriele@gmail.com>
2018-03-30 14:27:18 +0100

    seeder for env production

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,12 +14,14 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(RegionsTableSeeder::class);
-        factory('App\Course', random_int(4, 20))->create(['user_id' => 2])->each(function($u){
-            $u->partecipants()->saveMany(factory('App\Partecipant', random_int(0, 10))->create());
-        });
-        factory('App\Course', random_int(4, 10))->create(['user_id' => 1])->each(function($u){
-            $u->partecipants()->saveMany(factory('App\Partecipant', random_int(0, 10))->create());
-        });
-        factory('App\Newsletter', 100)->create();
+        if(env('APP_ENV') !== 'production'){
+            factory('App\Course', random_int(4, 20))->create(['user_id' => 2])->each(function($u){
+                $u->partecipants()->saveMany(factory('App\Partecipant', random_int(0, 10))->create());
+            });
+            factory('App\Course', random_int(4, 10))->create(['user_id' => 1])->each(function($u){
+                $u->partecipants()->saveMany(factory('App\Partecipant', random_int(0, 10))->create());
+            });
+            factory('App\Newsletter', 100)->create();
+        }
     }
 }
