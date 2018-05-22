bf8eb3b70ad72a611da6d38daa5866a274c30e54
Gabriele Soldati <soldati.gabriele@gmail.com>
2018-05-22 23:40:00 +0100

    update

--- app/Helpers/Telegram.php
+++ app/Helpers/Telegram.php
@@ -18,7 +18,7 @@ class Telegram
         $chat_id = '31019486';
         $disable_notification = (env('APP_ENV') === 'testing')? 'true' : 'false';
         
-        if(!$chat_id){
+        if($chat_id == null){
             return json_encode(['error' => 'no chat id selected']);
         }
         $url = env('TELEGRAM_URI') . env('TELEGRAM_TOKEN') . '/sendMessage?&disable_notification=' . $disable_notification . '&parse_mode=markdown&&chat_id=' . $chat_id . '&text=' . urlencode($text);
