4a937afcd634b2f0b7c17e550f1b4b5e28f0f5c3
Gabriele Soldati <soldati.gabriele@gmail.com>
2018-10-24 18:18:45 +0100

    migrations

---
+++ database/migrations/2018_10_24_170331_create_coupons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->text('description')->nullable;
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
