7a1499aeba9f91388222133f7e71f92f62735343
Gabriele Soldati <soldati.gabriele@gmail.com>
2019-03-13 18:11:44 +0100

    Removed API part

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,1 @@
 <?php
-
-use Illuminate\Http\Request;
-
-Route::group(['middleware' => 'ApiAuth'], function () {
-
-    Route::get('/partecipant', function (Request $request) {
-        return 'authorizzato';}
-    );
-
-    Route::apiResources(['courses' => 'CoursesController']);
-    Route::apiResources(['partecipants' => 'PartecipantsController']);
-
-    Route::get('getsubscriptions/{user_id?}', 'SubscriptionsController@getSubscriptions');
-    Route::any('subscribe', 'SubscriptionsController@subscribe');
-    Route::post('unsubscribe', 'SubscriptionsController@unsubscribe');
-});
