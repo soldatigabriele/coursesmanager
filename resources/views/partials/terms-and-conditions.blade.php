72e2d28354d681ee80b187a07cb6a92e5946db9e
Gabriele Soldati <soldati.gabriele@gmail.com>
2019-10-07 19:08:23 +0100

    update terms and conditions

--- resources/views/partials/terms-and-conditions.blade.php
+++ resources/views/partials/terms-and-conditions.blade.php
@@ -8,23 +8,25 @@
       </div>
       <div class="modal-body">
         <div id="full-width">
-  <!--content-->
-    <p style="text-align: center;">ASSOCIAZIONE CULTURALE<br>
-NUOVE PROSPETTIVE<br>
-via del Carso, 28 – 34078 Sagrado (GO)<br>
-C.F. 91033080317<br>
-____________________________</p>
-<p style="text-align: justify;"><strong>CONDIZIONI GENERALI PER LE ATTIVITÀ ORGANIZZATE DALLA NOSTRA ASSOCIAZIONE</strong></p>
-<p style="text-align: justify;">La quota di partecipazione riportata nelle locandine si riferisce al contributo spese richiesto per l’evento in questione. Con il primo corso/seminario/evento che si frequenta nell’anno solare corrente, si diventa automaticamente soci dell’Associazione Culturale Nuove Prospettive, la cui quota associativa di 50€ verrà evidenziata nella regolare ricevuta rilasciata agli interessati.</p>
-<p style="text-align: justify;">Tale quota è da ritenersi già compresa nel costo del primo corso/seminario/evento dell’anno solare.</p>
-<p style="text-align: justify;">Per gli eventi successivi, la quota di partecipazione riportata nelle locandina si riferisce esclusivamente all’evento in questione e quindi non si deve scorporare la quota associativa.</p>
-<p style="text-align: justify;">Diventando soci dell’Associazione si gode inoltre della copertura assicurativa di responsabilità civile nei confronti di terzi.</p>
-<p style="text-align: justify;">Una volta compilata la scheda di partecipazione, quando richiesto, bisogna effettuare il versamento della caparra e mandarci una mail con l’avvenuto pagamento. <strong>L’iscrizione viene confermata solo quando ci arriva la mail con la copia della ricevuta. Vi verrà inviata a quel punto una risposta di conferma.</strong></p>
-<p style="text-align: justify;"><strong>Nel caso di ritiro</strong>, la quota versata a scopo confirmatorio è rimborsabile o permutabile ESCLUSIVAMENTE se sarà formalmente inviata una mail di disdetta entro e non oltre 15 giorni dall’inizio del corso; verranno comunque trattenuti 20€ per ogni quota versata quali spese amministrative. Non è previsto alcun rimborso nel caso di ritiro successivo al termine.</p>
-<p style="text-align: justify;">Il cambio di corso è ammesso, se espressamente richiesto con una mail entro 15 giorni dall’inizio del corso; non è ammesso successivamente.<br>
-Nel caso in cui il corso sia sospeso/annullato, l’importo versato sarà restituito totalmente.</p>
-<p style="text-align: justify;">Qualche giorno prima del corso/seminario verrà inviata una mail a tutti gli iscritti con gli ultimi dettagli e <em>‘chi viene, da dove’</em> in modo da potersi accordare per gli spostamenti con le auto riducendo il numero di veicoli in movimento e limitare il nostro impatto sull’ambiente.</p>
-
+          <div style="text-align:center">
+          <p>ASSOCIAZIONE CULTURALE<br /> NUOVE PROSPETTIVE<br /> via del Carso, 28 &ndash; 34078 Sagrado (GO)<br /> C.F. 91033080317<br /> ____________________________</p>
+          <p><strong></strong></p>
+          </div>
+          <p><strong>CONDIZIONI GENERALI PER LE ATTIVIT&Agrave; ORGANIZZATE DALLA NOSTRA ASSOCIAZIONE</strong></p>
+          <p>La quota di partecipazione riportata nelle locandine si riferisce al contributo spese richiesto per l&rsquo;evento in questione. Con il primo corso/seminario/evento che si frequenta nell&rsquo;anno solare corrente, si diventa automaticamente soci dell&rsquo;Associazione Culturale Nuove Prospettive, la cui quota associativa di 50&euro; verr&agrave; evidenziata nella regolare ricevuta rilasciata agli interessati.</p>
+          <p></p>
+          <p>Tale quota &egrave; da ritenersi gi&agrave; compresa nel costo del primo corso/seminario/evento dell&rsquo;anno solare.</p>
+          <p>Per gli eventi successivi, la quota di partecipazione riportata nella locandina si riferisce esclusivamente all&rsquo;evento in questione e quindi non si deve scorporare la quota associativa.</p>
+          <p></p>
+          <p>Una volta compilata la scheda di partecipazione, quando richiesto, bisogna effettuare il versamento della caparra e mandarci una mail con l&rsquo;avvenuto pagamento.<strong>L&rsquo;iscrizione viene confermata solo quando ci arriva la mail con la copia della ricevuta. Vi verr&agrave; inviata a quel punto una risposta di conferma.</strong></p>
+          <p><strong></strong></p>
+          <p><strong>Nel caso di ritiro</strong>, la quota versata a scopo confirmatorio &egrave; rimborsabile o permutabile ESCLUSIVAMENTE se sar&agrave; formalmente inviata una mail di disdetta entro e non oltre i 15 giorni antecedenti all&rsquo;inizio del corso; verranno comunque trattenuti 20&euro; per ogni quota versata quali spese amministrative. Non &egrave; previsto alcun rimborso nel caso di ritiro successivo al termine.</p>
+          <p>Il cambio di corso &egrave; ammesso, se espressamente richiesto con una mail entro i 15 giorni antecedenti all&rsquo;inizio del corso; non &egrave; ammesso successivamente.</p>
+          <p></p>
+          <p>Nel caso in cui il corso sia sospeso/annullato, l&rsquo;importo versato sar&agrave; restituito totalmente.</p>
+          <p>Qualche giorno prima del corso/seminario verr&agrave; inviata una mail a tutti gli iscritti con gli ultimi dettagli e<em>&lsquo;chi viene, da dove&rsquo;</em>in modo da potersi accordare per gli spostamenti con le auto riducendo il numero di veicoli in movimento e limitare il nostro impatto sull&rsquo;ambiente.</p>
+          <p></p>
+          <p></p>
   </div>
       </div>
       <div class="modal-footer">
