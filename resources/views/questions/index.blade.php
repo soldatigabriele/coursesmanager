1031fc245ec815ee082fb8874260b7dd5e88fd17
Gabriele Soldati <soldati.gabriele@gmail.com>
2020-03-17 21:10:40 +0000

    fix style

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -6,56 +6,56 @@
     width:96%;
     margin:auto;
 }
+.card {
+    margin-bottom: 20px;
+}
 </style>
 @endsection
 
 @section('content')
 <div class="content">
-    <div class="row justify-content-center">
-        @foreach($courses as $course)
-
+    @foreach($courses as $course)
         <div class="card">
-        <div class="card-header">
-            <h3>{{ $course->long_id }}</h3>
-        </div>
-        <div class="card-body">
-            <div>
-                {{ route('questions-create', [ 'course_id' => $course->id, 'qn' => 3, ]) }}
+            <div class="card-header">
+                <h3>{{ $course->long_id }}</h3>
             </div>
-            <table class="table">
-                <thead>
-                    <th>#</th>
-                    <th>Nome</th>
-                    <th>Data</th>
-                    <th>Domande</th>
-                    <th>Feedback</th>
-                </thead>
-                @foreach($course->questions()->get() as $question)
-                <tr>
-                    <td> {{ $question->id }} </td>    
-                    <td> {{ $question->name}} {{ $question->surname }} </td>    
-                    <td> {{ $question->created_at->format('d/m/Y H:i')}} </td>    
-                    <td> 
-                        <ul>
-                            @foreach($question->questions as $q)
-                            <li>{{ $q }} </li>
-                            @endforeach
-                        </ul>
-                    </td>    
-                    <td> 
-                        <ul>
-                            @foreach($question->feedback as $q)
-                            <li>{{ $q }} </li>
-                            @endforeach
-                        </ul>
-                    </td>    
-                </tr>
-                @endforeach
-            </table>
+            <div class="card-body">
+                <div>
+                    {{ route('questions-create', [ 'course_id' => $course->id, 'qn' => 3, ]) }}
+                </div>
+                <table class="table">
+                    <thead>
+                        <th>#</th>
+                        <th>Nome</th>
+                        <th>Data</th>
+                        <th>Domande</th>
+                        <th>Feedback</th>
+                    </thead>
+                    @foreach($course->questions()->get() as $question)
+                    <tr>
+                        <td> {{ $question->id }} </td>    
+                        <td> {{ $question->name}} {{ $question->surname }} </td>    
+                        <td> {{ $question->created_at->format('d/m/Y H:i')}} </td>    
+                        <td> 
+                            <ul>
+                                @foreach($question->questions as $q)
+                                <li>{{ $q }} </li>
+                                @endforeach
+                            </ul>
+                        </td>    
+                        <td> 
+                            <ul>
+                                @foreach($question->feedback as $q)
+                                <li>{{ $q }} </li>
+                                @endforeach
+                            </ul>
+                        </td>    
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
         </div>
-        </div>
-        @endforeach
-    </div>
+    @endforeach
 </div>
 @endsection
 
