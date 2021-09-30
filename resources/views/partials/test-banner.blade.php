538e1d1b5aff019a26ae0a2aa7502394c1a4331a
Gabriele Soldati <soldati.gabriele@gmail.com>
2021-09-30 19:42:26 +0100

    Update dependencies

--- resources/views/partials/test-banner.blade.php
+++ resources/views/partials/test-banner.blade.php
@@ -1,4 +1,4 @@
-@if(config('app.debug'))
+@if(config('app.env') !== 'production')
     <div style="background:#00ff00;color:white;text-align:center;">
         TEST SERVER - TEST SERVER - TEST SERVER - TEST SERVER - TEST SERVER - TEST SERVER - TEST SERVER - TEST SERVER - TEST SERVER - TEST SERVER - TEST SERVER - TEST SERVER - TEST SERVER - 
     </div>
